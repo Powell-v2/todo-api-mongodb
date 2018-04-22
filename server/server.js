89d451c30f2a01378f47f2854d887cb9f1a40c4d
Paul Ermolin <oatto.paul@gmail.com>
2018-04-22 18:20:43 +0200

    - added some basic validation

--- server/server.js
+++ server/server.js
@@ -5,29 +5,42 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 const Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true,
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false,
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null,
   },
 });
 
-const dinnerTodo = new Todo({
-  text: 'Cook dinner',
+// const workoutTodo = new Todo({
+//   text: 'Work out',
+// });
+
+// workoutTodo.save()
+//   .then(doc => console.log('Saved todo:', doc))
+//   .catch(err => console.log('Unable to create todo :(', err))
+
+const User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+  },
 });
 
-dinnerTodo.save()
-  .then(doc => console.log('Saved todo:', doc))
-  .catch(err => console.log('Unable to create todo :(', err))
-
-const workoutTodo = new Todo({
-  text: 'Work out',
-  completed: false,
+const userMay = new User({
+  email: 'may@hotmail.com',
 });
 
-workoutTodo.save()
-  .then(doc => console.log('Saved todo:', doc))
-  .catch(err => console.log('Unable to create todo :(', err))
+userMay.save()
+  .then(doc => console.log('Saved user:', doc))
+  .catch(err => console.log('Unable to create user :(', err))
