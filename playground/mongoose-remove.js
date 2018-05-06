733072b032d86137c86fffb8bc040a20fbb41c1b
Paul Ermolin <oatto.paul@gmail.com>
2018-05-06 13:30:57 +0200

    - added DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,23 @@
+const { ObjectID } = require('mongodb');
+
+const { mongoose } = require('../server/db/mongoose');
+const { Todo } = require('../server/models/todo');
+const { User } = require('../server/models/user');
+
+// Todo.remove({})
+//   .then((result) => {
+//     console.log(result);
+//   });
+
+// Todo.findOneAndRemove
+
+Todo.findOneAndRemove({ _id: '5aeee39be95e946f5f4d1cca' })
+  .then((todo) => {
+    console.log(todo);
+  });
+
+
+Todo.findByIdAndRemove('5aeee39be95e946f5f4d1cca')
+  .then((todo) => {
+    console.log(todo);
+  });
