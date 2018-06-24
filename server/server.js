defb6febe33be6487979812d8fe7713e5d87a933
Paul Ermolin <oatto.paul@gmail.com>
2018-06-24 12:19:45 +0200

    - added DELETE /users/me/token route (for logout)

--- server/server.js
+++ server/server.js
@@ -51,6 +51,12 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token)
+    .then(() => res.status(200).send())
+    .catch(() => res.status(400).send())
+})
+
 app.get('/todos', (req, res) => {
   Todo.find({})
     .then(todos => res.send({
